64af9fff9a8b47252072cb3e85eca1d815ba4e86
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-09-03 11:25:41 -0400

    chore: Add Streamlit template page

---
+++ pages/template_pages.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import dotenv
+import os
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+dotenv.load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(openai_api_key=api_key)
+
+system_template_message = """
+You are a geography teacher.
+"""
+
+user_template_message = """
+What is the capital of {country}?
+"""
+
+prompt_template = ChatPromptTemplate.from_messages(
+    messages=[
+        ("system", system_template_message),
+        ("user", user_template_message),
+    ]
+)
+
+# El StrOutputParser se puede cambiar según el tipo de output que se espere
+chain = prompt_template | llm | StrOutputParser()
+
+
+st.markdown("# Crea tu evaluación")
+
+country_selected = st.selectbox(
+    "Selecciona un país",
+    [
+        "Argentina",
+        "Brasil",
+        "Chile",
+        "Colombia",
+        "Ecuador",
+        "México",
+        "Perú",
+        "Venezuela",
+    ],
+)
+
+template_prompt_input = {
+    "country": country_selected,
+}
+
+respuesta_generada = chain.invoke(template_prompt_input)
+
+st.write("Respuesta generada:", respuesta_generada)
