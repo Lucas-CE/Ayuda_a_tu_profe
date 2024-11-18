7d6cdc4f7b1200f7c9a219f60c7c5367c45cc58a
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-11-18 08:23:47 -0300

    docs: update documentation

--- app/Ayuda_a_tu_profe!.py
+++ app/Ayuda_a_tu_profe!.py
@@ -1,27 +1,33 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 st.title("Ayuda a tu profe! 🧑‍🏫")
 
-st.markdown(
-    """
-    ## ¿Qué es Ayuda a tu profe! 🧑‍🏫?
-    
-    Aquí los profesores nos entenderan.
-    
-    Todos aquí hemos rabiado alguna vez con crear evaluaciones, buscar bibliografía, actualizar el curso, etc.
-    """
-)
+col1, col2 = st.columns([0.6, 2])
 
-st.image("app/elprofe.jpg", width=300)
+with col1:
+    st.image("app/elprofe.jpg")
 
-st.markdown(
-    """
-    Pues ya no más, en Ayuda a tu profe! 🧑‍🏫 encontrarás herramientas que te facilitarán la vida
-    
-    Ayuda a tu profe! 🧑‍🏫 es una plataforma web que disponibiliza un amplio set de herramientas que permitiran a los profesores 
-    mejorar su desempeño en la docencia.
-    
-    Además, esta aplicación es escalable, por la que se podrían integrar otras herramientas en ella.
-    
-    """
-)
+with col2:
+    st.markdown(
+        """
+        Sabemos lo cansador que es crear evaluaciones, buscar bibliografía,
+        actualizar el curso, etc, y todo eso sin contar con todo el trabajo que
+        implica preocuparse de que tus estudiantes aprendan.
+
+        Es por esto que hemos creado **Ayuda a tu profe! 🧑‍🏫**, una plataforma
+        web que disponibiliza un amplio set de herramientas que permitiran a
+        los profesores mejorar su desempeño en la docencia.
+
+        Actualmente la aplicación cuenta con 3 modulos que buscan alivianarle
+        la pega a los profesores:
+
+        - **Generador de evaluaciones**: Permite crear evaluaciones de manera
+        rápida solo usando información de tus clases.
+        - **Buscar bibliografía**: Encuentra referencias bibliográficas de manera
+        sencilla.
+        - **Actualización curricular**: Actualiza tu curso de manera automática
+        con la información más novedosa.
+        """
+    )
